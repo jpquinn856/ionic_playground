5e3e09aaaaa79ed1fbc4ea51da6e2c04e5cbf725
Brennan Batalla <brennan.batalla@gmail.com>
2018-05-19 17:36:53 -0400

    First Commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'login',
+  templateUrl: 'login.html'
+})
+export class loginPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
